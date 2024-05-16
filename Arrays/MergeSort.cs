8f17a863c1d809fc32777acee0d65a01a1686a70
SoumiaSAADOUN <es_saadoun@esi.dz>
2024-05-16 14:25:42 +0100

    BST implementation

---
+++ Arrays/MergeSort.cs
@@ -0,0 +1,21 @@
+class MergeSort
+{
+    static void swap(int[] arr, int i, int j)
+    {
+        if (i < 0 || j < 0 || i >= att.Length || j >= arr.Length)
+            throw new ArgumentOutOfRangeException('Index out of range!');
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+    }
+
+
+
+
+    static void MergeSort(int[] arr1, int[] arr2)
+    {
+        int[] result = new int[]();
+        
+    }
+}
---
+++ Trees/BinaryTrees/BST/SearchBTS.cs
@@ -0,0 +1,85 @@
+class Node
+{
+    public int val;
+    public Node left;
+    public Node right;
+    public Node(int val)
+    {
+        this.val = val;
+
+    }
+
+    public Node SearchValInBinarySearchTree(Node root, int val)
+    {
+        if (root == null || root.val == val)
+            return root;
+        else if (root.val > val)
+            return SearchValInBinarySearchTree(root.left, val);
+        else
+            return SearchValInBinarySearchTree(root.right, val);
+
+
+    }
+
+    public Node InsertIntoBinarySearchTree(Node root, int val)
+    {
+        if (root == null)
+        {
+            return new Node(val);
+        }
+        if (root.val > val)
+            root.left = InsertIntoBinarySearchTree(root.left, val);
+
+        else
+            root.right = InsertIntoBinarySearchTree(root.right, val);
+        return root;
+    }
+
+    public Node DeleteValFromBinarySearchTree(Node root, int val)
+    {
+        if (root == null)
+        {
+            return root;
+        }
+
+        if (root.val > val)
+            root.right = DeleteValFromBinarySearchTree(root.right, val);
+        else if (root.val < val)
+            root.left = DeleteValFromBinarySearchTree(root.left, val);
+        else
+        {
+            // case that the node has only right child
+            if (root.left == null)
+            {
+                root = root.right;
+                return root;
+            }
+            // case that the node has only left child
+            if (root.right == null)
+            {
+                root = root.left;
+                return root;
+            }
+
+            // case that the node has 2 children, we replace it with the biggest val of its left child
+            Node maxValue = GetMaxValueInBinarySearchTree(root.left);
+            root.val = maxValue.val;
+
+            root.left = DeleteValFromBinarySearchTree(root.left, maxValue.val);
+            
+        }
+
+        return root;
+    }
+
+    public Node GetMaxValueInBinarySearchTree(Node root)
+    {
+        Node current = root;
+
+        while (current != null && current.right != null)
+        {
+            current = current.right;
+        }
+        return current;
+    }
+}
